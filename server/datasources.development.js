fe1031efcdb3cb7785fe75e7296fdbde360da480
Blake Dykes <blakedykes0104@gmail.com>
2020-09-09 12:46:03 -0500

    Created docker containers for api & mongo as well as docker-compose.yml

--- server/datasources.development.js
+++ server/datasources.development.js
@@ -1,11 +1,11 @@
 module.exports = {
  mongodb: {
    connector: 'mongodb',
-   hostname: process.env.DB_HOST,
-   port: process.env.DB_PORT,
-   user: process.env.DB_USER,
-   password: process.env.DB_PASSWORD,
-   database: process.env.DB_NAME,
+   hostname: process.env.DB_HOST || 'localhost',
+   port: process.env.DB_PORT || 27017,
+   user: process.env.DB_USER || '',
+   password: process.env.DB_PASSWORD || '',
+   database: process.env.DB_NAME || 'lafs',
    url: process.env.DB_URL
  }
 };
